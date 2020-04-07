05d89bac6f88b55d9f078c5e795574dae2c58894
John Jackson <jjackson0308@gmail.com>
2020-04-07 18:25:21 +0100

    Additions to the CHELSA extraction script to include faster raster processing from exactextractr and for UCloud runs.
    Adding in the buffers of 50m and 5km radius to the LPI study coordinates.

--- chelsa_extraction_pilot.R
+++ chelsa_extraction_pilot.R
@@ -10,16 +10,26 @@
 rm(list = ls())
 options(width = 100)
 
+# general
 library(tidyverse)
 library(grid)
 library(gridExtra)
+
+# spatial
+library(sp)
+library(rgeos)
 library(raster)
+library(exactextractr)
+library(rgdal)
+library(sf)
 
 ##__________________________________________________________________________________________________
 #### 1. Set up data ####
 
+#_________________________________________________
 ## 1a. Mammal living planet index data
-load("../mam.RData", verbose = T)
+load("mam.RData", verbose = T) # source different in the UCloud
+load("../rawdata/mam.RData", verbose = T)
 rm(mam_meta) # don't need the meta-data here
 
 # 5 or more years of data and CHELSA limits
@@ -31,18 +41,25 @@ mam <- mam %>%
          year >= 1979 & year <= 2013) %>%
   dplyr::select(ID, Binomial, Latitude, Longitude, year)
 
-## 1b. CHELSA raster file lists (via network drive) as a tibble
-netdr_t <- "/Volumes/PlantAnimalBiodemography/Climate/chelsa/temp/"
-netdr_p <- "/Volumes/PlantAnimalBiodemography/Climate/chelsa/prec/"
+#__________________________________________________
+## 1b. CHELSA raster file lists
 
-files_temp <- paste(netdr_t, list.files(netdr_t), sep = "/")
-files_precip <- paste(netdr_p, list.files(netdr_p), sep = "/")
+# from the network drive
+# netdr_t <- "/Volumes/PlantAnimalBiodemography/Climate/chelsa/temp/"
+# netdr_p <- "/Volumes/PlantAnimalBiodemography/Climate/chelsa/prec/"
+
+# from the UCloud - you select the files you want to use when creating the job
+U_t <- "CHELSA/temperature"
+U_p <- "CHELSA/precipitation"
+
+files_temp <- paste(U_t, list.files(U_t), sep = "/")
+files_precip <- paste(U_p, list.files(U_p), sep = "/")
 
 files_df <- tibble(files_temp, files_precip) %>% 
   mutate(year = map_int(files_temp, ~ as.integer(strsplit(.x, "_")[[1]][3]))) %>% 
   mutate(month = map_int(files_temp, ~ as.integer(strsplit(.x, "_")[[1]][4])))
 
-## 1c. Merge species-coord data with files_df data
+## 1c. Merge species-coordinate data with files_df data
 mam_coord <- mam %>% 
   full_join(x = ., y = files_df, by = "year") %>% 
   mutate(obj_temp = paste0("temp_",year,"_",month),
@@ -70,13 +87,45 @@ mam_chelsa <- bind_rows(lapply(X = 1:nrow(files_df), FUN = function(x){
   proj4string(c_species) = pbase
   c_species = spTransform(c_species, crs(chelsa_temp))
   
+  #_________________________________________________________________________________________________
+  # 3a. Extract the climate data from exact points- Takes a while
   cells_sp = cellFromXY(chelsa_temp, c_species)
-
-  # 3. Extract the climate data - Takes a while
   c_species$raster_cell = cells_sp
   c_species$temp = chelsa_temp[cells_sp] / 10 - 273.15 # Kelvin to celcius
   c_species$precip = chelsa_precip[cells_sp]
   c_species$precip[c_species$precip > 65000] = NA_real_}
+  
+  #_________________________________________________________________________________________________
+  # 3b. Extracting the climate data from a small buffer radius of 50m - if ~identical will use this
+  ## Key advantage is using exactextractr, which uses C++ coding to do raster operations much faster.
+  ## First convert to the Universal Transverse Mercator projection system to buffer in m accurately:
+  ## Useful information on projection systems at https://rspatial.org/raster/spatial/6-crs.html
+  p_utm = CRS("+proj=utm +datum=WGS84")
+  utm = spTransform(c_species, p_utm)
+  # do it by the study ID for future reference
+  # 50 line segments for the approximation of a circle
+  csp_buff_small = rgeos::gBuffer(utm, width = 50, quadsegs = 50,
+                             byid = TRUE, id = utm$ID)
+  ## Convert back to WGS84 to extract from CHELSA and convert to sf type
+  csp_buff_wgs_small = sf::st_as_sf(spTransform(csp_buff_small, 
+                                       CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+  c_species$temp_50m = (exact_extract(x = chelsa_temp, y = csp_buff_wgs_small, 
+                                      fun = "mean", progress = FALSE)/10) - 273.15
+  c_species$precip_50m = exact_extract(x = chelsa_precip, y = csp_buff_wgs_small, 
+                                       fun = "mean", progress = FALSE)
+  c_species$precip_50m[c_species$precip_50m > 65000] = NA_real_
+  
+  #_________________________________________________________________________________________________
+  # 3c. Large buffer radius of 5km - More biologically relevant?
+  csp_buff = rgeos::gBuffer(utm, width = 5000, quadsegs = 50,
+                                  byid = TRUE, id = utm$ID)
+  csp_buff_wgs = sf::st_as_sf(spTransform(csp_buff, 
+                                          CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+  c_species$temp_5km = (exactextractr::exact_extract(x = chelsa_temp, y = csp_buff_wgs, 
+                                                     fun = "mean", progress = FALSE)/10) - 273.15
+  c_species$precip_5km = exactextractr::exact_extract(x = chelsa_precip, y = csp_buff_wgs, 
+                                                      fun = "mean", progress = FALSE)
+  c_species$precip_5km[c_species$precip_50m > 65000] = NA_real_
   
   # 4. Return
   cat("\r", "Your job is ",round(x/nrow(files_df) *100, 2), "% complete          ")
@@ -85,20 +134,7 @@ mam_chelsa <- bind_rows(lapply(X = 1:nrow(files_df), FUN = function(x){
 }))
 Sys.time() - x
 
-# ### Buffer method
-# coordinates(xy) <- ~ x + y
-# proj4string(xy) <- "+init=epsg:4326"
-# 
-# ## create 100-km buffer
-# xy_utm = spTransform(xy, CRS("+init=epsg:32621"))
-# gbf_utm = rgeos::gBuffer(xy_utm, width = 1e5, quadsegs = 250L)
-# gbf = spTransform(gbf_utm, CRS(proj4string(xy)))
-# 
-# ## extract values and calculate weighted mean
-# vls = extract(WOA, gbf, weights = TRUE)[[1]]
-# sum(vls[, 1] * vls[, 2])
-# # [1] 4.18117
-
 ##__________________________________________________________________________________________________
 #### 3. Save ####
-save(mam_chelsa, file = "data/mam_chelsa.RData")
+save(mam_chelsa, file = "lpi_weather_pilot/mam_chelsa.RData")
+
