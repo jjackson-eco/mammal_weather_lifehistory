47233b4631a7496be7ed41aafaa8abb89ea2f132
John Jackson <jjackson0308@gmail.com>
2020-05-05 17:34:40 +0100

    Adding summary variables for time-series gaps

--- annual_abundance_changes/timeseries_gap_exploration.R
+++ annual_abundance_changes/timeseries_gap_exploration.R
@@ -29,21 +29,59 @@ mam <- mam %>%
   filter(n >= 5) %>% 
   dplyr::select(-n)
 
+# Check for duplicated years
+# dupyer <- mam %>% 
+#   group_by(ID) %>% 
+#   summarise(Binomial = Binomial[1],
+#             duplicate_years = n() -n_distinct(year))
+
 ##__________________________________________________________________________________________________
-#### 2. Time series gaps for each study ####
+#### 2. Testing - Fossa  ####
 
-dupyer <- mam %>% 
-  group_by(ID) %>% 
-  summarise(Binomial = Binomial[1],
-            duplicate_years = n() -n_distinct(year))
+test <- filter(mam, ID == 25396)
 
-mam %>% 
+ggplot(test, aes(x = year, y = scaled_abundance)) + 
+  geom_point()
+
+tyear <- test$year
+diff_tyear <- diff(tyear)
+cumsum_blocks <- cumsum(c(1, diff_tyear != 1))
+
+no_consecutive_blocks <- n_distinct(cumsum_blocks)
+prop_1year_transitions <- sum(diff_tyear == 1)/ length(diff_tyear)
+longest_block <- max(table(cumsum_blocks))
+
+# # interesting function
+# consecutive_blocks <-  length(split(tyear, cumsum(c(1, diff(tyear) != 1))))
+
+##__________________________________________________________________________________________________
+#### 3. Exploring gaps for all studies ####
+
+mam_gaps <- mam %>% 
   group_by(ID) %>% 
   group_modify(~{
     cyears = .$year
+    diff_cyears = diff(cyears)
+    cumsum_blocks = cumsum(c(1, diff_cyears != 1))
+    
+    summarise(., Binomial = Binomial[1],
+              study_length = length(cyears),
+              no_consecutive_blocks = n_distinct(cumsum_blocks),
+              prop_1year_transitions = sum(diff_cyears == 1)/ length(diff_cyears),
+              longest_block = max(table(cumsum_blocks)))
   })
 
+##__________________________________________________________________________________________________
+#### 4. Exploration plots ####
 
+ggplot(mam_gaps, aes(x = prop_1year_transitions)) +
+  geom_histogram(bins = 10, fill = "lightblue", 
+                 colour = "black", size = 0.1) +
+  labs(x = "Proportion of study in 1-year transitions",
+       y = "Number of studies") +
+  theme_bw(base_size = 11) +
+  ggsave("plots/annual_abundance/Proportion_1year_transitions.jpeg",
+         width = 4, height = 3, units = "in", dpi = 400)
 
 
 
