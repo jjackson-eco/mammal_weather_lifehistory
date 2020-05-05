fdae87c65c9924139f09bc5da7a52ac4b864eea5
John Jackson <jjackson0308@gmail.com>
2020-05-05 15:39:18 +0100

    Adding skewness and kurtosis of weather anomalies and number of consecutive odd months.

--- weather_variables/annual_weather_variables.R
+++ weather_variables/annual_weather_variables.R
@@ -14,6 +14,7 @@ library(tidyverse)
 library(grid)
 library(gridExtra)
 library(psych)
+library(moments)
 
 chelsa_stl <- readRDS("data/chelsa_stl.RDS") 
 
@@ -22,25 +23,28 @@ chelsa_stl <- readRDS("data/chelsa_stl.RDS")
 
 mam_chelsa_annual <- chelsa_stl %>% 
   group_by(ID, scale) %>% 
+  # adding summary stats for 8 and 9 for the full time series - odd values
   mutate(temp_anomaly_sd = sd(temp_anomaly),
          temp_anomaly_mean = mean(temp_anomaly),
          precip_anomaly_sd = sd(precip_anomaly),
-         precip_anomaly_mean = mean(precip_anomaly)) %>% # adding summary stats for (6) for the full time series
+         precip_anomaly_mean = mean(precip_anomaly),
+         temp_odd = temp_anomaly_mean + (2*temp_anomaly_sd),
+         precip_odd = precip_anomaly_mean + (2*precip_anomaly_sd)) %>% 
   ungroup() %>% 
   group_by(ID, year, scale) %>% 
   summarise(Binomial = Binomial[1], 
             Longitude = Longitude[1], 
             Latitude = Latitude[1],
             
-            # 1) Mean climate variable
+            # 1) Mean climate variable - raw central tendency
             mean_temp = mean(temp),
             mean_precip = mean(precip),
             
-            # 2) Mean anomaly
+            # 2) Mean anomaly - central tendency
             mean_temp_anomaly = mean(temp_anomaly),
             mean_precip_anomaly = mean(precip_anomaly),
             
-            # 3) Mean absolute anomaly
+            # 3) Mean absolute anomaly - central tendency
             mean_abtemp_anomaly = mean(abs(temp_anomaly)),
             mean_abprecip_anomaly = mean(abs(precip_anomaly)),
             
@@ -54,14 +58,25 @@ mam_chelsa_annual <- chelsa_stl %>%
             # 5) Anomaly variance
             temp_anomaly_variance = var(temp_anomaly),
             precip_anomaly_variance = var(precip_anomaly),
-
-            # 6) no. odd months
-            num_odd_months_temp = length(which(abs(temp_anomaly) > 
-                                                temp_anomaly_mean + (2*temp_anomaly_sd))),
-            num_odd_months_precip = length(which(abs(precip_anomaly) > 
-                                                 precip_anomaly_mean + (2*precip_anomaly_sd)))) %>% 
+            
+            # 6) Anomaly skewness - Symmetry of the distribution
+            temp_anomaly_skewness = skewness(temp_anomaly),
+            precip_anomaly_skewness = skewness(precip_anomaly),
+            
+            # 7) Anomaly Kurtosis - weight of the tails relative to the rest of the distribution - not peakedness
+            temp_anomaly_kurtosis = kurtosis(temp_anomaly),
+            precip_anomaly_kurtosis = kurtosis(precip_anomaly),
+            
+            # 8) no. odd months - extreme values
+            num_odd_months_temp = length(which(abs(temp_anomaly) > temp_odd[1])),
+            num_odd_months_precip = length(which(abs(precip_anomaly) > precip_odd[1])),
+            
+            # 9) no. consecutive odd months - extreme values
+            num_consecutive_odd_months_temp = 
+              length(which(diff(which(abs(temp_anomaly) > temp_odd[1])) == 1)),
+            num_consecutive_odd_months_precip = 
+              length(which(diff(which(abs(precip_anomaly) > precip_odd[1])) == 1))) %>% 
   ungroup()
-
 
 ##__________________________________________________________________________________________________
 #### 2. Plots ####
@@ -85,7 +100,9 @@ ggplot(ts_ID28, aes(x = date)) +
         panel.grid.minor = element_blank()) +
   ggsave(filename = "plots/developing_annual_weather_variables/ID28_anomaly_sd.jpeg",
          width = 7, height = 4, units = "in", dpi = 400)
-  
+
+
+#________________________________________________________
 # Pairs plot of annual measures
 
 jpeg(filename = "plots/developing_annual_weather_variables/temp_annual_weather_correlations.jpeg",
@@ -103,16 +120,7 @@ dev.off()
 ##__________________________________________________________________________________________________
 #### 2. Save ####
 
-saveRDS(mam_chelsa_annual, file = "data/mam_chelsa_annual.RDS") # <---- need to add precip
-
-
-
-
-
-
-
-
-
+saveRDS(mam_chelsa_annual, file = "data/mam_chelsa_annual.RDS")
 
 
 
