52042e53c473b236e801fe18ff8f5e089871c995
John Jackson <jjackson0308@gmail.com>
2020-05-13 18:18:23 +0100

    Plot additions exploring some of the features of the linear detrending of abundance timeseries + saving the data.

--- annual_abundance_changes/linear_detrend_annual_abundance.R
+++ annual_abundance_changes/linear_detrend_annual_abundance.R
@@ -45,12 +45,45 @@ mam_detrend <- mammal %>%
   }) %>% 
   ungroup()
 
+# Look very normally distributed - linear relationship seems justified
+ggplot(mam_detrend, aes(x = residual_abundance)) +
+  geom_histogram(bins = 30, colour = "black", size = 0.1,
+                 fill = "lightblue") +
+  labs(x = "Residual scaled abundance", y = "Frequency") +
+  facet_wrap(~Order, scales = "free_y", ncol = 5) +
+  theme_bw(base_size = 22) +
+  ggsave("plots/annual_abundance/Residaul_abundance_histogram.jpeg",
+         width = 14, height = 12, units = "in", dpi = 400)
 
+##__________________________________________________________________________________________________
+#### 3. Rough look at coefficients across taxa ####
 
+mam_detrend %>% 
+  group_by(Family) %>% 
+  summarise(mn_coef = mean(coef),
+            se_coef = sd(coef)/sqrt(n())) %>% 
+  ungroup() %>% 
+  arrange(desc(mn_coef)) %>% 
+  mutate(Family = factor(Family, levels = .$Family)) %>% 
+  ggplot(aes(x = Family, y = mn_coef)) +
+  geom_hline(yintercept = 0, size = 0.5, linetype = "dashed") +
+  geom_errorbar(aes(ymax = mn_coef + se_coef,
+                    ymin = mn_coef - se_coef),
+                width = 0.01) +
+  geom_point() +
+  labs(y = "Mean coefficient of linear abundance trend") +
+  coord_flip() +
+  theme_bw(base_size = 17) +
+  theme(axis.text.y = element_text(size = 12)) +
+  ggsave("plots/annual_abundance/linear_abundance_coefficients.jpeg",
+         width = 9, height = 13, units = "in", dpi = 400)
 
+##__________________________________________________________________________________________________
+#### 4. Saving data ####
 
+mam_detrend <- dplyr::select(mam_detrend, -c(raw_abundance,scaled_abundance))
 
-
+save(mam_detrend, file = "data/mam_detrend.RData")
 
 
 
