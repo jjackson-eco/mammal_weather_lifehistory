d9d8939ae788d538d7f908147994f0a3ccf03d5f
John Jackson <jjackson0308@gmail.com>
2020-05-28 18:01:34 +0100

    rename to look specifically at testing teh utility of timeseries models

--- annual_abundance_changes/timeseries_models_test.R
+++ annual_abundance_changes/timeseries_models_test.R
@@ -74,7 +74,8 @@ ggsave(grid.arrange(acf_plot, propsig_plot, ncol = 1),
 
 # Fitting AR models for all ID_blocks and returning model selection data
 mam_ARmodels <- mam_IDblocks %>% 
-  filter(ID_block != "11175_1" & ID_block != "10734_1") %>% 
+  # Have to exclude these studies that have stange problem that prevents ML being fit - exp growth???
+  filter(ID_block != "11175_1" & ID_block != "10734_1" & ID_block != "4803_1") %>% 
   group_by(ID_block) %>% 
   group_modify(~{
     #convert abundance to a timeseries
@@ -89,20 +90,38 @@ mam_ARmodels <- mam_IDblocks %>%
            AIC_AR = AIC(mod_AR), AIC_WN = AIC(mod_WN),
            n_obs = length(cts))
   }) %>% ungroup() %>% 
-  mutate(dAIC = AIC_AR - AIC_WN)
+  mutate(dAIC = AIC_AR - AIC_WN,
+         n_gr = n_obs - (n_obs %% 5))
 
-
-
+hist(mam_ARmodels$dAIC)
 
 for(i in unique(mam_IDblocks$ID_block)){
   print(i)
-  if(i != "11175_1" & i != "10734_1"){
+  if(i != "11175_1" & i != "10734_1" & i != "4803_1"){
   cdat = filter(mam_IDblocks, ID_block == i)
   cts = ts(cdat$log_abundance)
   mod_AR = arima(cts, order = c(1,0,0), method = "ML")}
 }
 
+ggplot(mam_ARmodels, aes(y = dAIC, x = factor(n_gr), 
+                         colour = factor(n_gr), 
+                         fill = factor(n_gr))) +
+  geom_hline(yintercept = -2) +
+  geom_violin(alpha = 0.5) +
+  geom_jitter(width = 0.2, alpha = 0.7) +
+  scale_colour_viridis_d(end = 0.8, guide = F,
+                         aesthetics = c("colour", "fill")) +
+  labs(x = "Years of record (grouped by 5-year bins)", y = "Change in AIC from AR(1) to WN") +
+  annotate("text", x = 1, y = -90, hjust = 0,
+           label = paste(round(length(which(mam_ARmodels$dAIC <= -2))/nrow(mam_ARmodels) * 100, 1),
+                         "% of timeseries records\nhave AIC difference <= -2")) +
+  theme_bw(base_size = 12) +
+  ggsave("plots/annual_abundance/AIC_AR.jpeg",
+         width = 8, height = 5, units = "in",
+         dpi = 400)
 
+# 30% of studies have support from the predictive performance of an AR(1) model
+length(which(mam_ARmodels$dAIC <= -2))/nrow(mam_ARmodels)
 
 
 
