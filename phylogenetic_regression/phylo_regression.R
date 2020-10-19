3f4048ea0ec6d03268db2e35fe50de35948b1dd1
John Jackson <jjackson0308@gmail.com>
2020-10-19 18:29:36 +0200

    simple model selection for temperature

--- phylogenetic_regression/phylo_regression.R
+++ phylogenetic_regression/phylo_regression.R
@@ -8,7 +8,8 @@
 ##                                                ##
 ####################################################
 
-## Species level variation and phylogenetic signal for mean temp/precip anomalies at 5km resolution
+## Species level variation, phylogenetic signal and spatial effects,
+## for mean temp/precip anomalies at 5km resolution
 
 rm(list = ls())
 options(width = 100)
@@ -18,6 +19,7 @@ library(ape)
 library(brms)
 library(nlme)
 library(patchwork)
+library(ggridges)
 
 ##____________________________________________________________________________________________________________________________________________________________________________________________________________
 #### 1. Load data ####
@@ -130,10 +132,10 @@ precip_test <- brm(
   data2 = list(A_precip = A_precip),
   prior = c(
     prior(normal(0, 0.5), class =  Intercept),
-    prior(exponential(15), class = sd, group = "spp"),
+    prior(exponential(3), class = sd, group = "spp"),
     prior(exponential(3), class = sd, group = "species"),
     prior(exponential(3), class = sigma)),
-  control = list(adapt_delta = 0.95),
+  control = list(adapt_delta = 0.99),
   chains = 1, iter = 4000, warmup = 500
 )
 
@@ -157,11 +159,15 @@ plot(precip_test)
 # plot(temp_testOU)
 
 ##____________________________________________________________________________________________________________________________________________________________________________________________________________
-#### 6. Full model tests ####
+#### 6. Full models ####
 
-# Simple phylogenetic model
+##___________________________________________________________________________
+## 6a. Temperature 
+
 set.seed(666)
-temp_mod <- brm(
+
+# Base model
+temp_base <- brm(
   coef_temp ~ 1 + (1|gr(spp, cov = A_temp)) + (1| species), 
   data = mam_temp, family = gaussian(), 
   data2 = list(A_temp = A_temp),
@@ -170,8 +176,97 @@ temp_mod <- brm(
     prior(exponential(1), class = sd, group = "spp"),
     prior(exponential(1), class = sd, group = "species"),
     prior(exponential(1), class = sigma)),
-  control = list(adapt_delta = 0.95),
-  chains = 1, iter = 4000, warmup = 500
+  control = list(adapt_delta = 0.97),
+  chains = 4, cores = 4, iter = 2000, warmup = 500
 )
 
+# biome
+temp_biome <- brm(
+  coef_temp ~ 1 + biome + (1|gr(spp, cov = A_temp)) + (1| species), 
+  data = mam_temp, family = gaussian(), 
+  data2 = list(A_temp = A_temp),
+  prior = c(
+    prior(normal(0, 0.5), class =  Intercept),
+    prior(normal(0, 0.5), class =  b),
+    prior(exponential(1), class = sd, group = "spp"),
+    prior(exponential(1), class = sd, group = "species"),
+    prior(exponential(1), class = sigma)),
+  control = list(adapt_delta = 0.99),
+  chains = 4, cores = 4, iter = 2000, warmup = 500
+)
+
+# latitude
+temp_lat <- brm(
+  coef_temp ~ 1 + lat + (1|gr(spp, cov = A_temp)) + (1| species), 
+  data = mam_temp, family = gaussian(), 
+  data2 = list(A_temp = A_temp),
+  prior = c(
+    prior(normal(0, 0.5), class =  Intercept),
+    prior(normal(0, 0.5), class =  b),
+    prior(exponential(1), class = sd, group = "spp"),
+    prior(exponential(1), class = sd, group = "species"),
+    prior(exponential(1), class = sigma)),
+  control = list(adapt_delta = 0.97),
+  chains = 4, cores = 4, iter = 2000, warmup = 500
+)
+
+## Model comparisons
+temp_base <- add_criterion(temp_base, criterion = c("loo", "waic"))
+temp_biome <- add_criterion(temp_biome, criterion = c("loo", "waic"))
+temp_lat <- add_criterion(temp_lat, criterion = c("loo", "waic"))
+
+## Biome model looks promising
+loo_compare(temp_base, temp_biome, temp_lat, criterion = "loo")
+
+# Pareto k outliers
+temp_pareto <- as.data.frame(loo(temp_base, pointwise = TRUE)$pointwise)
+
+mam_temp %>% 
+  mutate(pareto = temp_pareto$influence_pareto_k) %>% 
+  ggplot(aes(x = coef_temp, y = pareto)) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  geom_text(aes(label = ID, colour = pareto), show.legend = F) +
+  labs(x = "Linear temperature coefficient", y = "Pareto k estimate") +
+  theme_bw(base_size = 14) + theme(panel.grid = element_blank()) +
+  ggsave(filename = "plots/phylogenetic_regression/repeated_obs_spatial/pareto_k_temp.jpeg",
+         width = 6, height = 6, units = "in", dpi = 400)
+
+# biome 2 - no bat Myotis_emarginatus
+mam2 <- filter(mam_temp, spp != "Myotis_emarginatus")
+mamMCC2 <- keep.tip(mamMCC_temp, mam2$spp)
+A_temp2 <- ape::vcv.phylo(mamMCC2)
+
+temp_biome <- brm(
+  coef_temp ~ 1 + biome + (1|gr(spp, cov = A_temp2)) + (1| species), 
+  data = mam2, family = gaussian(), 
+  data2 = list(A_temp2 = A_temp2),
+  prior = c(
+    prior(normal(0, 0.5), class =  Intercept),
+    prior(normal(0, 0.5), class =  b),
+    prior(exponential(1), class = sd, group = "spp"),
+    prior(exponential(1), class = sd, group = "species"),
+    prior(exponential(1), class = sigma)),
+  control = list(adapt_delta = 0.99),
+  chains = 4, cores = 4, iter = 2000, warmup = 500
+)
+
+biome_post <- posterior_samples(temp_biome, pars = "biome")
+colnames(biome_post) <- c("Desert", "Flooded.grassland", "Mediterranean", "Montane", 
+                          "Temperate.broadleaf", "Temperate.coniferous","Temperate.grassland", 
+                          "Tropical.coniferous", "Tropical.broadleaf", "Tropical.grassland", 
+                          "Tropical.moist", "Tundra")
+
+
+## Plot the biome effects out
+tibble(biome_post, sim = 1:nrow(biome_post)) %>% 
+  pivot_longer(-sim, names_to = "biome", values_to = "post") %>%
+  mutate(biome = gsub("[.]", " ", biome)) %>% 
+  ggplot(aes(x = post, y = biome, fill = stat(x))) +
+  geom_vline(xintercept = 0) +
+  geom_density_ridges_gradient(scale = 1.1) +
+  scale_fill_viridis_c(option = "C", guide = F) +
+  labs(x = "Posterior temperature effect", y = NULL) +
+  theme_ridges(center_axis_labels = TRUE, font_size = 20, grid = F) +
+  ggsave(filename = "plots/phylogenetic_regression/repeated_obs_spatial/biome_effects.jpeg",
+         width = 7, height = 10, units = "in", dpi = 500)
 
