8b95a17017061d5673d3e455b6177b0d6826f9b0
John Jackson <jjackson0308@gmail.com>
2020-11-04 16:32:47 +0100

    Shift to included the updated framework for the species names with taxize

--- weather_population_growth/linear_effects_phylogenetic_plots.R
+++ weather_population_growth/linear_effects_phylogenetic_plots.R
@@ -28,18 +28,20 @@ load("../Phlogenetics/mam_lpd_pruned.RData", verbose = TRUE)
 ## Weather effects
 load("data/pgr_weather/mnanom_5km.RData", verbose = TRUE)
 
+## Species names
+load("../rawdata/GBIF_species_names_mamUPDATE.RData", verbose = TRUE)
+
 ##__________________________________________________________________________________________________
 #### 2. Merging and dropping tips ####
 
 ## Getting weather effects for each checked species name
 mam_res_names <- mnanom_5km %>% 
   ungroup() %>% 
-  dplyr::select(ID, Order, coef_temp, coef_precip) %>% 
+  dplyr::select(ID, Order, Binomial, coef_temp, coef_precip) %>% 
   left_join(x = ., 
-            y = dplyr::select(LPD_tree_update, ID, checked_speciesname),
-            by = "ID") %>% 
-  mutate(checked_speciesname = gsub(" ", "_", checked_speciesname)) %>% 
-  group_by(checked_speciesname) %>% 
+            y = dplyr::select(lpd_gbif, Binomial, gbif.species.tree, gbif.species.id.tree),
+            by = "Binomial") %>% 
+  group_by(gbif.species.tree) %>% 
   summarise(Order = Order[1],
             coef_temp = mean(coef_temp), 
             coef_precip = mean(coef_precip),
@@ -53,14 +55,15 @@ mam_res_names <- mnanom_5km %>%
          coef_precip_fac = cut(coef_precip, breaks = c(-2, seq(-0.5,0.5,length = 7), 2),
                                labels = c("-2 to -0.5", "-0.5 to -0.33", "-0.33 to -0.17", "-0.17 to 0",
                                           "0 to 0.17", "0.17 to 0.33", "0.33 to 0.5", "0.5 to 2")),
-         genus_label = gsub("_.*", "", checked_speciesname))
+         genus_label = gsub("_.*", "", gbif.species.tree),
+         gbif.species.tree2 = gsub(" ", "_", gbif.species.tree))
   
 ## Dropping tips
-mamMCC_weatheff <- keep.tip(mamMCC_pruned, mam_res_names$checked_speciesname)
+mamMCC_weatheff <- keep.tip(mamMCC_pruned, mam_res_names$gbif.species.tree2)
 
 ## Adding in weather effects and Order columns by converting to a tbl_tree and treedata object
 tree_plotdat <- as_tibble(mamMCC_weatheff) %>% 
-  full_join(x = ., y = mam_res_names, by = c("label" = "checked_speciesname")) %>% 
+  full_join(x = ., y = mam_res_names, by = c("label" = "gbif.species.tree2")) %>% 
   as.treedata()
 
 ##__________________________________________________________________________________________________
@@ -83,7 +86,7 @@ temp_hist <- ggplot(mam_res_names, aes(x = coef_temp_fac, fill = coef_temp_fac))
 # the tree
 temp_tree <- ggtree(tree_plotdat, aes(colour = coef_temp_fac), layout = "circular", size = 0.6) +
     geom_tippoint(aes(colour = coef_temp_fac), size = 5,show.legend = FALSE, offset = 2) +
-    geom_tiplab(aes(label = genus_label), size = 3, colour = "black", offset = 5) +
+    geom_tiplab(aes(label = genus_label), size = 2, colour = "black", offset = 5) +
     scale_color_brewer(palette = "Spectral",direction = -1, na.value = "black", guide = F)
 
 ggdraw() +
@@ -109,17 +112,14 @@ precip_hist <- ggplot(mam_res_names, aes(x = coef_precip_fac, fill = coef_precip
 # the tree
 precip_tree <- ggtree(tree_plotdat, aes(colour = coef_precip_fac), layout = "circular", size = 0.6) +
   geom_tippoint(aes(colour = coef_precip_fac), size = 5,show.legend = FALSE, offset = 2) +
-  geom_tiplab(aes(label = genus_label), size = 3, colour = "black", offset = 5) +
+  geom_tiplab(aes(label = genus_label), size = 2, colour = "black", offset = 5) +
   scale_color_brewer(palette = "Spectral",direction = -1, na.value = "black", guide = F) 
 
 ggdraw() +
   draw_plot(precip_tree) +
   draw_plot(precip_hist, x = 0.4, y = 0.32, width = 0.39, height = 0.18) +
-  ggsave("plots/weather_pop_growth/mam_precip_tree.pdf",
-         height = 11, width = 11, units = "in")
-
-rm(list=ls())
-options(width = 100)
+  ggsave("plots/weather_pop_growth/mam_precip_tree.jpeg",
+         height = 11, width = 11, units = "in", dpi = 400)
 
 ##__________________________________________________________________________________________________
 #### 4. Coefficient plot ####
